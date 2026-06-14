fcca1d13c24906f6a6d285f01e5c179690cd36f8
2HJ <2hyechin@gmail.com>
2026-06-14 22:43:23 +0000

    수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React, { useState, useRef, useEffect } from "react";
 // ==========================================
 // ❄️ 1단계: 겨울 폭풍 대비하기
 // ==========================================
-function Step1({ onNextStep, scale }) {
+function Step1({ onNextStep, scale }: any) {
   const [matches, setMatches] = useState({
     pipe: false,
     table: false,
@@ -191,7 +191,7 @@ function Step1({ onNextStep, scale }) {
 // ==========================================
 // 🏕️ 2단계: 체온 유지하기
 // ==========================================
-function Step2({ onNextStep, scale }) {
+function Step2({ onNextStep, scale }: any) {
   const [appliedItems, setAppliedItems] = useState({
     clothes: false,
     cocoa: false,
